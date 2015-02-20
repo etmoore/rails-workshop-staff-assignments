b99ef2fcb992f57154fac15a9c2a650511f0d5b6
Evan Moore <etmoore@gmail.com>
2015-02-20 14:16:53 -0700

    users must enter a first name or title for a person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,15 @@
 class Person < ActiveRecord::Base
 
-def full_name
-  "#{title} #{first_name} #{last_name}"
-end
+  validate :first_name_or_title
+
+  def full_name
+    "#{title} #{first_name} #{last_name}"
+  end
+
+  def first_name_or_title
+    if (title.nil? || title.empty?) && (first_name.nil? || first_name.empty?)
+      errors.add(:title, " or First name must be entered")
+    end
+  end
 
 end
--- spec/features/person_spec.rb
+++ spec/features/person_spec.rb
@@ -37,14 +37,40 @@ feature 'Person' do
     end
 
     click_on 'Edit'
-    fill_in 'Title', :with => "Dr."
-    fill_in 'First name', :with => 'Funken'
-    fill_in 'Last name', :with => 'Stein'
+    fill_in 'Title', with: "Dr."
+    fill_in 'First name', with: 'Funken'
+    fill_in 'Last name', with: 'Stein'
     click_on 'Update User'
 
     within '.table' do
       expect(page).to have_content('Dr. Funken Stein')
     end
+  end
+
+  scenario "Users must enter a title/last_name or first_name/last_name" do
+    create_user email: "user@example.com"
+    person = create_person
+
+    visit root_path
+    click_on "Login"
+
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    click_on person.full_name
+    within 'h1' do
+      expect(page).to have_content(person.full_name)
+    end
+
+    click_on 'Edit'
+    fill_in 'Title', with: ''
+    fill_in 'First name', with: ''
+    fill_in 'Last name', with:  'Stein'
+    click_on 'Update User'
+
+    expect(page).to have_content('Title or First name must be entered')
+
   end
 
 end
