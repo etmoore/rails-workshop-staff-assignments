ea0151970d81e30fd30de2a39a831ff01224700d
Evan Moore <etmoore@gmail.com>
2015-02-20 13:18:52 -0700

    added create_person object creation method

--- spec/support/object_creation_methods.rb
+++ spec/support/object_creation_methods.rb
@@ -3,6 +3,14 @@ def create_user(overrides = {})
     name: 'Some User',
     email: 'user@example.com',
     password: 'password',
-    password_confirmation: 'password'
+    password_confirmation: 'password',
+  }.merge(overrides))
+end
+
+def create_person(overrides = {})
+  Person.create!({
+    title: Faker::Name.prefix,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
   }.merge(overrides))
 end
